045d128599277a8c041e845e94276749ea62cd18
orelrafa <orelrafailov1998@gmail.com>
2024-03-26 11:19:41 +0300

    removed redundant console.log and comment made by me

--- routes/about_route.js
+++ routes/about_route.js
@@ -4,9 +4,7 @@ const Dev = require("../models/devs_model.js");
 
 router.get("/", async (req, res) => {
   try {
-    //collection.Find(criteria).SetFields(Fields.Include("oneField", "anotherField").Exclude("_id"))
-    const devs = await Dev.find({}, {'_id': false});
-    console.log(devs)
+    const devs = await Dev.find({}, { _id: false });
     res.status(200).json(devs);
   } catch (err) {
     res.status(500).json({ error: err.message });
